380017ed1709a37eb09c1be25941d48320eb312a
maksutyilmaz23 <maksutyilmaz23@gmail.com>
2026-08-01 21:31:08 +0300

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'hesap-defterim-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // App'in kendi dosyaları için cache-first, dış CDN'ler (Chart.js, jsPDF vb.) için ağdan yükle
+  const url = new URL(event.request.url);
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request))
+    );
+  }
+});
